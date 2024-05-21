8dc25ae60309d8bdafb9a2110d8ed1e2218f5a45
youngreal <59333182+youngreal@users.noreply.github.com>
2024-05-22 01:04:14 +0900

    feat: SpringBoot 프로젝트 초기세팅

---
+++ tera-backend-test/src/test/java/com/example/terabackendtest/TeraBackendTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.terabackendtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeraBackendTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
