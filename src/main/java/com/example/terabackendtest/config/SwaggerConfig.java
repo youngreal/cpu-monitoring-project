63675d688038e7c050ed8e74de45b88869e0f726
youngreal <59333182+youngreal@users.noreply.github.com>
2024-05-26 02:52:34 +0900

    feat: Swagger 적용
    * Controller에 침투되는 Swagger 관련코드를 별도의 interface로 옮겨 책임을 분리 해보았다.

---
+++ src/main/java/com/example/terabackendtest/config/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package com.example.terabackendtest.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+
+	@Bean
+	public OpenAPI openAPI() {
+		return new OpenAPI()
+			.info(new Info()
+				.title("Cpu Monitoring API Document")
+				.description("Cpu Monitoring API Document")
+				.version("v0.1"));
+	}
+}
--- src/main/java/com/example/terabackendtest/controller/dto/CpuUsageResponse.java
+++ src/main/java/com/example/terabackendtest/controller/dto/CpuUsageResponse.java
@@ -1,14 +1,15 @@
 package com.example.terabackendtest.controller.dto;
 
-import java.util.List;
+import java.time.LocalDateTime;
 
 import com.example.terabackendtest.dto.CpuDto;
 
 public record CpuUsageResponse(
-	List<CpuDto> cpuDto
+	int usagePercent,
+	LocalDateTime timestamp
 ) {
 
-	public static CpuUsageResponse from(List<CpuDto> cpuDtos) {
-		return new CpuUsageResponse(cpuDtos);
+	public static CpuUsageResponse from(final CpuDto cpuDto) {
+		return new CpuUsageResponse(cpuDto.usagePercent(), cpuDto.timestamp());
 	}
 }
